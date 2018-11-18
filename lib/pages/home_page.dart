72e519966a9572340283003d7f39d74fb1a26fff
Yonca Coskun <yoncacoskun@Yoncas-MacBook-Pro.local>
2018-11-18 13:33:06 +0200

    commit admob

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,10 @@ import 'package:droztips/pages/list_page.dart';
 import 'package:droztips/widgets/custom_tab.dart';
 import 'package:droztips/widgets/favorite_button.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_admob/firebase_admob.dart';
+
+//const APP_ID = "ca-app-pub-9868823063887131~4682191364";
+const APP_ID = "ca-app-pub-9868823063887131~4103892275";
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,22 +15,64 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
+  //admob
+  static final MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
+    testDevices: APP_ID != null ? [APP_ID] : null,
+    keywords: ['Games', 'Puzzles'],
+  );
+  BannerAd bannerAd;
+  InterstitialAd interstitialAd;
+
+  BannerAd buildBanner() {
+    return BannerAd(
+        adUnitId: BannerAd.testAdUnitId,
+        size: AdSize.banner,
+        listener: (MobileAdEvent event) {
+          print(event);
+        });
+  }
+
+  InterstitialAd buildInterstitial() {
+    return InterstitialAd(
+        adUnitId: InterstitialAd.testAdUnitId,
+        targetingInfo: targetingInfo,
+        listener: (MobileAdEvent event) {
+          print(event);
+        });
+  }
+
+//admob
+
   TabController controller;
 
   @override
   void initState() {
     super.initState();
     controller = new TabController(length: 3, vsync: this);
+
+    //admob
+    FirebaseAdMob.instance.initialize(appId: FirebaseAdMob.testAppId);
+    bannerAd = buildBanner()..load();
+    interstitialAd = buildInterstitial()..load();
+    //admob
   }
 
   @override
   void dispose() {
+    //admob
+    bannerAd?.dispose();
+    interstitialAd?.dispose();
+    //admob
+
     controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    bannerAd
+      ..load()
+      ..show();
     return new Scaffold(
       appBar: new AppBar(
         backgroundColor: Color.fromRGBO(102, 181, 138, 1.0),
